5d550d07a98403fcace96be70debef77a290418d
juanMaAV92 <juan.armero@y.uno>
2023-07-17 15:19:53 -0500

    list_based_queue and node_based_queue

---
+++ programming_concepts/data_structures/list_based_queue.py
@@ -0,0 +1,23 @@
+
+class ListQueue:
+
+    def __init__(self) -> None:
+        self.items = []
+        self.size = 0
+
+    
+    def enqueue( self, data):
+        self.items.insert(0, data)
+        self.size += 1
+
+    
+    def dequeue( self ):
+        data = self.items.pop()
+        self.size -= 1
+        return data
+    
+    def traverse(self):
+        total_items = self.size
+
+        for item in range(total_items):
+            print(self.items[item])
