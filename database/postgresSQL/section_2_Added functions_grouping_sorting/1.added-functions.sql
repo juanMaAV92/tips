16f2d5325440c725151d74d56f052e9fa001fdf3
Juan Manuel Armero Viveros <juan.armero@y.uno>
2023-11-27 11:15:39 -0500

    section 2 Done

--- database/postgresSQL/section_2_Added functions_grouping_sorting/1.added-functions.sql
+++ database/postgresSQL/section_2_Added functions_grouping_sorting/1.added-functions.sql
@@ -1,1 +1,66 @@
 SELECT * FROM users;
+
+-- Operador BETWEEN
+SELECT 
+    first_name, 
+    last_name, 
+    followers 
+FROM 
+    users 
+WHERE 
+    -- followers > 4600 and followers < 4700
+    followers between 4600 and 4700
+order by
+    followers desc;
+
+-- Funciones de agregación
+SELECT 
+    count(*) as total_users, 
+    MIN(followers) as min_followers,
+    MAX(followers) as max_followers,
+    ROUND(AVG(followers)) as avg_followers,
+    SUM(followers)/count(*) as avg_followers_manual
+FROM 
+    users;
+
+-- Group by
+SELECT
+    count(*),
+    followers
+FROM
+    users
+WHERE
+    followers = 4 or followers = 4999
+GROUP BY
+    followers
+ORDER BY
+    followers desc;
+
+-- having
+SELECT
+    count(*),
+    country
+FROM
+    users
+GROUP BY
+    country
+HAVING
+    count(*) > 5
+ORDER BY
+    country asc;
+
+-- distinct, muestra valores unicos no repetidos
+SELECT distinct country FROM users;
+
+-- consulta para extraer el dominio de los correos
+SELECT
+    SUBSTRING(email, POSITION('@' in email) + 1) as domain
+FROM
+    users
+group by
+    domain
+HAVING
+    count(*) > 1;  
+
+
+
---
+++ database/postgresSQL/section_2_Added functions_grouping_sorting/exercises/3.homework.sql
@@ -0,0 +1,112 @@
+
+-- 1. Cuantos usuarios tenemos con cuentas @google.com
+-- Tip: count, like
+SELECT
+    count(*)
+FROM
+    users
+WHERE
+    email like '%@google.com';
+
+
+-- 2. De qué países son los usuarios con cuentas de @google.com
+-- Tip: distinct
+SELECT
+    distinct country
+FROM
+    users
+WHERE
+    email like '%@google.com';
+
+-- 3. Cuantos usuarios hay por país (country)
+-- Tip: Group by
+SELECT
+    count(*),
+    country 
+FROM
+    users
+GROUP BY
+    country;
+
+
+-- 4. Listado de direcciones IP de todos los usuarios de Iceland
+-- Campos requeridos first_name, last_name, country, last_connection
+SELECT
+    first_name,
+    last_name,
+    country,
+    last_connection
+FROM
+    users
+WHERE
+    country = 'Iceland';
+
+-- 5. Cuantos de esos usuarios (query anterior) tiene dirección IP
+-- que inicia en 112.XXX.XXX.XXX
+SELECT
+    count(*)
+FROM
+    users
+WHERE
+    country = 'Iceland' and last_connection like '112.%';
+
+
+-- 6. Listado de usuarios de Iceland, tienen dirección IP
+-- que inicia en 112 ó 28 ó 188
+-- Tip: Agrupar condiciones entre paréntesis 
+SELECT
+    first_name,
+    last_name,
+    country,
+    last_connection
+FROM
+    users
+WHERE
+    country = 'Iceland' and (last_connection like '112.%' or last_connection like '28.%' or last_connection like '188.%');
+
+
+-- 7. Ordene el resultado anterior, por apellido (last_name) ascendente
+-- y luego el first_name ascendentemente también
+
+SELECT
+    first_name,
+    last_name,
+    country,
+    last_connection
+FROM
+    users
+WHERE
+    country = 'Iceland' and (last_connection like '112.%' or last_connection like '28.%' or last_connection like '188.%')
+ORDER BY
+    last_name asc, first_name asc
+
+-- 8. Listado de personas cuyo país está en este listado
+-- ('Mexico', 'Honduras', 'Costa Rica')
+-- Ordenar los resultados de por País asc, Primer nombre asc, apellido asc
+-- Tip: Investigar IN
+-- Tip2: Ver Operadores de Comparación en la hoja de atajos (primera página)
+SELECT
+    first_name,
+    last_name,
+    country
+FROM
+    users
+WHERE
+    country IN ('Mexico', 'Honduras', 'Costa Rica')
+ORDER BY
+    country asc, first_name asc, last_name asc
+
+
+-- 9. Del query anterior, cuente cuántas personas hay por país
+-- Ordene los resultados por País asc
+SELECT
+    count(*),
+    country
+FROM
+    users
+WHERE
+    country IN ('Mexico', 'Honduras', 'Costa Rica')
+GROUP BY
+    country
+ORDER BY
+    country asc
